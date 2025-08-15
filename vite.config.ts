c22fdf1d698c89177e29fb38913a71300fddc779
timoteostifft <timoteostifft@gmail.com>
2025-08-15 13:29:27 -0300

    fix: vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -9,14 +9,24 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          ui: ["@radix-ui/react-slot", "@radix-ui/react-separator"],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
   },
 }));
